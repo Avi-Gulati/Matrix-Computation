a3ba51669f9839f40fef69fe3f86554c02f4122c
Avi Gulati <avigulati@dhcp-10-250-117-93.harvard.edu>
2023-03-26 00:54:06 -0400

    I finished writing the non-optimized Strassen (ie: the bare bones). I have not tested it yet

--- mm.cc
+++ mm.cc
@@ -27,6 +27,129 @@ int** standard(int** A, int** B, int n) {
     return product;
 }
 
+int** matrix_addition(int** A, int** B, size_t n) {
+    
+    int** commputation = new int*[n];
+    for(size_t i = 0; i < n; ++i)
+        commputation[i] = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            product[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+int** matrix_subtraction(int** A, int** B, size_t n) {
+    
+    int** commputation = new int*[n];
+    for(size_t i = 0; i < n; ++i)
+        commputation[i] = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            product[i][j] = A[i][j] - B[i][j];
+        }
+    }
+}
+
+int** strassen(int** A, int** B, int n0) {
+
+    size_t n = sizeof(A[1]) / sizeof(int) // Get the dimensions of A and B at this point  
+    if (n == n0) {
+        return standard(A, B, n0);
+    }
+    
+    size_t ndiv_2 = n/2
+    int** a = new int*[ndiv_2];            // Creating the different spliced arrays 
+    int** b = new int*[ndiv_2];
+    int** c = new int*[ndiv_2];
+    int** d = new int*[ndiv_2];
+
+    int** e = new int*[ndiv_2];
+    int** f = new int*[ndiv_2];
+    int** g = new int*[ndiv_2];
+    int** h = new int*[ndiv_2];
+
+    for (size_t i = 0; i < ndiv_2; ++i)  // Dynamically allocate memory for array of integers with ndiv_2 elements 
+        a[i] = new int[ndiv_2];          // for each spliced array
+        b[i] = new int[ndiv_2];
+        c[i] = new int[ndiv_2];
+        d[i] = new int[ndiv_2];
+
+        e[i] = new int[ndiv_2];
+        f[i] = new int[ndiv_2];
+        g[i] = new int[ndiv_2];
+        h[i] = new int[ndiv_2];
+
+
+    for (size_t i = 0; i < ndiv_2; i++) {   // The next four for loops assign the spliced arrays to the 
+        for (size_t j = 0; j < ndiv_2; j++) { // correct values 
+            a[i][j] = A[i][j];
+            e[i][j] = B[i][j];
+        }
+    }
+    for (size_t i = 0; i < ndiv_2; i++) {
+        for (size_t j = ndiv_2; j < n; j++) {
+            b[i][j] = A[i][j];
+            f[i][j] = B[i][j];
+        }
+    }
+    for (size_t i = ndiv_2; i < n; i++) {
+        for (size_t j = 0; j < ndiv_2; j++) {
+            c[i][j] = A[i][j];
+            g[i][j] = B[i][j];
+        }
+    }
+    for (size_t i = ndiv_2; i < n; i++) {
+        for (size_t j = ndiv_2; j < n; j++) {
+            d[i][j] = A[i][j];
+            h[i][j] = B[i][j];
+        }
+    }
+
+    p1 = strassen(matrix_addition(a,d,ndiv_2), matrix_addition(e,h,ndiv_2));
+    p2 = strassen(d, matrix_subtraction(g,e,ndiv_2));
+    p3 = strassen(matrix_addition(a,b,ndiv_2), h);
+    p4 = strassen(matrix_subtraction(b,d,ndiv_2), matrix_addition(g,h,ndiv_2));
+    p5 = strassen(a, matrix_subtraction(f,h,ndiv_2));
+    p6 = strassen(matrix_addition(c,d,ndiv_2), e);
+    p7 = strassen(matrix_subtraction(a,c,ndiv_2), matrix_addition(e,f,ndiv_2));
+
+
+    c11 = matrix_addition(matrix_subtraction(matrix_addition(p1, p2,ndiv_2), p3), p4);
+    c12 = matrix_addition(p3, p5, ndiv_2);
+    c21 = matrix_addition(p6, p2, ndiv_2);
+    c22 = matrix_subtraction(matrix_subtraction(matrix_addition(p5, p1,ndiv_2), p6), p7);
+
+    int** C = new int*[n];
+    for(size_t i = 0; i < n; ++i)
+        C[i] = new int[n];
+
+    for (size_t i = 0; i < ndiv_2; i++) {   // The next four for loops assign the spliced arrays to the 
+        for (size_t j = 0; j < ndiv_2; j++) { // correct values 
+            C[i][j] = c11[i][j];
+        }
+    }
+    for (size_t i = 0; i < ndiv_2; i++) {
+        for (size_t j = ndiv_2; j < n; j++) {
+            C[i][j] = c12[i][j-ndiv_2];
+        }
+    }
+    for (size_t i = ndiv_2; i < n; i++) {
+        for (size_t j = 0; j < ndiv_2; j++) {
+            C[i][j] = c21[i-ndiv_2][j];
+        }
+    }
+    for (size_t i = ndiv_2; i < n; i++) {
+        for (size_t j = ndiv_2; j < n; j++) {
+            C[i][j] = c22[i][j-ndiv_2];
+        }
+    }
+
+    return C;
+}
+
 // prints matrix
 void print_matrix(int** matrix, int k, int n) {
     printf("matrix %i:\n", k);
