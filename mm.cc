a1d3806510f8ee80f154cadb4be2d770653d0127
Avi Gulati <avigulati@dhcp-10-250-117-93.harvard.edu>
2023-03-27 11:52:34 -0400

    With padding. passes 42/50 on autograder

--- mm.cc
+++ mm.cc
@@ -127,33 +127,24 @@ int** strassen(int** A, int** B, int n, int n0) {
     int** p6 = strassen(matrix_subtraction(b,d,ndiv_2), matrix_addition(g,h,ndiv_2), ndiv_2, 1);
     int** p7 = strassen(matrix_subtraction(c,a,ndiv_2), matrix_addition(e,f,ndiv_2), ndiv_2, 1);
 
-    int** c11 = matrix_addition(matrix_addition(matrix_subtraction(p4, p2, ndiv_2), p5, ndiv_2), p6, ndiv_2);
-    int** c12 = matrix_addition(p1, p2, ndiv_2);
-    int** c21 = matrix_addition(p3, p4, ndiv_2);
-    int** c22 = matrix_addition(matrix_addition(matrix_subtraction(p1, p3, ndiv_2), p5, ndiv_2), p7, ndiv_2);
-
     int** C = new int*[n];
     for(size_t i = 0; i < n; i++)
         C[i] = new int[n];
 
-    for (size_t i = 0; i < ndiv_2; i++) {   // The next four for loops assign the spliced arrays to the 
+    for (size_t i = 0; i < ndiv_2; i++) {   // The following for loops assign the spliced arrays to the 
         for (size_t j = 0; j < ndiv_2; j++) { // correct values 
-            C[i][j] = c11[i][j];
+            C[i][j] = p4[i][j] - p2[i][j] + p5[i][j] + p6[i][j];
         }
-    }
-    for (size_t i = 0; i < ndiv_2; i++) {
         for (size_t j = ndiv_2; j < n; j++) {
-            C[i][j] = c12[i][j-ndiv_2];
+            C[i][j] = p1[i][j-ndiv_2] + p2[i][j-ndiv_2];
         }
     }
     for (size_t i = ndiv_2; i < n; i++) {
         for (size_t j = 0; j < ndiv_2; j++) {
-            C[i][j] = c21[i-ndiv_2][j];
+            C[i][j] = p3[i-ndiv_2][j] + p4[i-ndiv_2][j];
         }
-    }
-    for (size_t i = ndiv_2; i < n; i++) {
         for (size_t j = ndiv_2; j < n; j++) {
-            C[i][j] = c22[i-ndiv_2][j-ndiv_2];
+            C[i][j] = p1[i-ndiv_2][j-ndiv_2] - p3[i-ndiv_2][j-ndiv_2] + p5[i-ndiv_2][j-ndiv_2] + p7[i-ndiv_2][j-ndiv_2];
         }
     }
 
@@ -165,18 +156,14 @@ int** strassen(int** A, int** B, int n, int n0) {
     matrix_del(p5, ndiv_2);
     matrix_del(p6, ndiv_2);
     matrix_del(p7, ndiv_2);
-    matrix_del(c11, ndiv_2);
-    matrix_del(c12, ndiv_2);
-    matrix_del(c21, ndiv_2);
-    matrix_del(c22, ndiv_2);
     return C;
 }
 
 // We used this helper function after looking at a Geeksforgeeks.org tutorial. 
 // We attribute credit to Geeksforgeeks. 
-bool isPowerOfTwo(int n) {
-    return (ceil(log2(n)) == floor(log2(n)));
-}
+//bool isPowerOfTwo(int n) {
+    //return (ceil(log2(n)) == floor(log2(n)));
+//}
 // Also taken online from includehelp.com. We use bitshifting to find the next power of 2 of a number. 
 int  computeNextPowerOfTwo( int  n )
 {
@@ -184,7 +171,7 @@ int  computeNextPowerOfTwo( int  n )
 	int value = 1;
 	// The following while loop will run until we 
 	// get a number greater than n
-	while(value<=n)
+	while(value<n)
 	{
 		// value will be left shifted by 1 place in each iteration
 		value=value << 1;
@@ -197,10 +184,8 @@ int main(int _argc, char *argv[]) {
     int mode = stoi(argv[1]);
     const int N = stoi(argv[2]);
     
-    int nextPowerOfTwo = N;  // initialized as N if N is already a power of 2. 
-    if (!isPowerOfTwo(N)) {
-        nextPowerOfTwo = computeNextPowerOfTwo(N);
-    }
+
+    int nextPowerOfTwo = computeNextPowerOfTwo(N);
     
     // initialize matrices (array of arrays)
     ifstream matrices(argv[3]);
@@ -244,16 +229,20 @@ int main(int _argc, char *argv[]) {
     }
 
     // print matrices
-    print_matrix(matrix1, 1, N);
-    print_matrix(matrix2, 2, N);
-    int** product1 = standard(matrix1, matrix2, N); // Commented out because the autograder does not do anything standard 
-    int** product2 = strassen(matrix1, matrix2, N, 1);
-    print_matrix(product1, 3, N);
-    print_matrix(product2, 4, N);
+    // print_matrix(matrix1, 1, nextPowerOfTwo);
+    // print_matrix(matrix2, 2, nextPowerOfTwo);
+    // int** product1 = standard(matrix1, matrix2, nextPowerOfTwo); // Commented out because the autograder does not do anything standard 
+    int** product2 = strassen(matrix1, matrix2, nextPowerOfTwo, 1);
+    // print_matrix(product1, 3, nextPowerOfTwo);
+    // print_matrix(product2, 4, nextPowerOfTwo);
+
+    for (int i = 0; i < N; i++) {
+        printf("%i\n",product2[i][i]);
+    }
 
     // delete matrices
     matrix_del(matrix1, N);
     matrix_del(matrix2, N);
-    matrix_del(product1, N);
+    // matrix_del(product1, N);
     matrix_del(product2, N);      
 }
